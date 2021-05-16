770fa19901bb4de36adbae8673210a90c3869478
mtalhagaoglu <47723198+mtalhagaoglu@users.noreply.github.com>
2021-05-16 21:04:12 +0300

    fix buton padding

--- src/ProgressSteps/ProgressButtons.js
+++ src/ProgressSteps/ProgressButtons.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView,View } from 'react-native';
 
 const ProgressButtons = props => (
-<SafeAreaView style={{ flexDirection: 'row', paddingHorizontal: 20, paddingVertical: 10 }}>
-<View style={{ flexGrow: 1, alignItems: 'flex-start' }}>
-  {props.renderPreviousButton()}
- </View>
-<View style={{ flexGrow: 1, alignItems: 'flex-end' }}>{props.renderNextButton()}
-</View>
-</SafeAreaView>
+  <SafeAreaView style={{ flexDirection: 'row', paddingHorizontal: 20, paddingVertical: 10 }}>
+    <View style={{ flexGrow: 1, alignItems: 'flex-start' }}>
+      {props.renderPreviousButton()}
+    </View>
+    <View style={{ flexGrow: 1, alignItems: 'flex-end' }}>{props.renderNextButton()}
+    </View>
+  </SafeAreaView>
 );
 
 export default ProgressButtons;
